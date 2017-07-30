85bc78f201f74d8378f3e0b0b927fd85495b9081
chrisdruta <chris.aturd@gmail.com>
2017-07-30 11:44:59 -0700

    Initial merge of config parser
    Config files can contain multiple url streams. The script now randomly selects a stream from the list of urls to generate the background image.

--- liveframe.py
+++ liveframe.py
@@ -1,16 +1,52 @@
-import sys
-import time
+import sys, configparser, random
 
 import livestreamer
-#import numpy as np
 import cv2
 
-# Getting stream source list
-streams = livestreamer.streams(sys.argv[1])
+def create_config(Config):
+	cfg = open('config', 'w')
 
-stream = streams['best']
+	Config.add_section('URLs')
+	Config.set('URLs', 'streamList', 'https://www.youtube.com/watch?v=cIL6cKQd1Ls')
 
-cap = cv2.VideoCapture(stream.url)
-frame = cap.read()
-cap.release()
-cv2.imwrite('background.png', frame[1])
+	Config.write(cfg)
+	cfg.close()
+
+def read_config(Config):
+	raw = Config.get('URLs', 'streamList')
+
+	urlList = list(filter(None, (x.strip() for x in raw.splitlines())))
+
+	streamList = []
+	for url in urlList:
+		try:
+			stream = livestreamer.streams(url)['best']
+			streamList.append(stream)
+		except:
+			print("Error: couldn't fetch video stream for url " + url)
+
+	return streamList
+
+
+# Config setup
+Config = configparser.ConfigParser()
+
+if not Config.read('config'):
+	 create_config(Config)
+
+streams = read_config(Config)
+
+if streams:
+	randSel = random.randint(0, len(streams) - 1)
+
+	stream = streams[randSel]
+
+	cap = cv2.VideoCapture(stream.url)
+	isFine, frame = cap.read()
+	cap.release()
+
+	if isFine:
+		cv2.imwrite('background.png', frame)
+
+else:
+	print("No streams available!")
